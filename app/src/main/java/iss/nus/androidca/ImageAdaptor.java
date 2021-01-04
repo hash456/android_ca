3c411b7cd20f2edb4221f8bb6c3e58f1f3ec79bd
IsabelCHM <63800687+IsabelCHM@users.noreply.github.com>
2021-01-04 18:43:24 +0800

    game page

---
+++ app/src/main/java/iss/nus/androidca/ImageAdaptor.java
@@ -0,0 +1,61 @@
+package iss.nus.androidca;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdaptor extends BaseAdapter {
+    private Context mContext;
+
+    public Integer[] mThumbIds;
+
+    // Constructor
+    public ImageAdaptor(Context c, Integer[] mThumbIds) {
+        mContext = c;
+        this.mThumbIds = mThumbIds;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public Integer[] getmThumbIds() {
+        return mThumbIds;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+
+        }
+        else
+        {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+
+
+    // Keep all Images in array
+
+}
